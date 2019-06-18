0c4513ca1b96d1f6432a466d557a7bc193827e41
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-06-18 17:06:05 -0400

    Ancestor problem completed.

---
+++ projects/ancestor/ancestor.py
@@ -0,0 +1,58 @@
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
+def earliest_ancestor(ancestors, person):
+    graph = {}
+    for pair in ancestors:
+        parent = pair[0]
+        child = pair[1]
+        if child in graph:
+            graph[child].add(parent)
+        else:
+            graph[child] = set()
+            graph[child].add(parent)
+    if person not in graph:
+        return -1
+    else:
+        def dft(graph, starting_vertex):
+            stack = Stack()
+            stack.push([starting_vertex])
+            visited = set()
+            paths = []
+            pathLengths = {}
+            while stack.size() > 0:
+                path = stack.pop()
+                node = path[-1]
+                if node not in visited and node in graph:
+                    visited.add(node)
+                    for parent in graph[node]:
+                        new_path = path[:]
+                        new_path.append(parent)
+                        stack.push(new_path)
+                elif node not in graph:
+                    paths.append(path)
+                    length = len(path)
+                    if length not in pathLengths:
+                        pathLengths[length] = 1
+                    else:
+                        pathLengths[length] += 1
+            maxLength = max(pathLengths)
+            if pathLengths[maxLength] == 1:
+                return paths[-1][-1]
+            else:
+                minimum = []
+                for subArray in paths:
+                    minimum.append(subArray[-1])
+                return min(minimum)
+        ancestor = dft(graph, person)
+        return ancestor
